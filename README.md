501c4d9574b3ae9d5db859e641c75c0c98efd17a
Luke Leber <lal65@psu.edu>
2022-08-15 16:07:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Penn State Online Education Design System: Pattern Lab Integration
+This repository exposes the [Penn State Online Education Design System](https://github.com/psu-online-education/components) through [Pattern Lab](https://patternlab.io/), a frontend
+workshop environment that helps build, view, test, and showcase the design
+system's UI components.
+
+## Hosting
+The most recent snapshot of the design system can be found at https://psu-online-education.github.io.
+
+## Development
+Any changes to the main branch will trigger a rebuild of the static site.  Please open pull requests to make any changes.
---
+++ alter-twig.php
@@ -0,0 +1,80 @@
+<?php
+
+use Twig\Environment;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+require_once('node_modules/@psu-online-education/smart-datetime/SmartDatetime.php');
+
+function moveKeyBefore($arr, $find, $move) {
+  if (!isset($arr[$find], $arr[$move])) {
+    return $arr;
+  }
+
+  $elem = [$move=>$arr[$move]];  // cache the element to be moved
+  $start = array_splice($arr, 0, array_search($find, array_keys($arr), TRUE));
+  unset($start[$move]);  // only important if $move is in $start
+  return $start + $elem + $arr;
+}
+
+function addCustomExtension(Environment $env) {
+  $env->addExtension(new SmartDatetimeExtension);
+  $env->addFilter(new TwigFilter('clean_unique_id', function($id) {
+    $id = str_replace(' ', '-', $id);
+    return $id . '-' . uniqid();
+  }));
+  $env->addFunction(new TwigFunction('get_component_stylesheets', function () {
+      $manifests = [];
+      foreach (glob('node_modules/@psu-online-education/*/package.json') as $manifest) {
+        $manifest_json = json_decode(file_get_contents($manifest), TRUE, 512, JSON_THROW_ON_ERROR);
+        $manifests[$manifest_json['name']] = $manifest_json;
+      }
+      unset($manifests['@psu-online-education/patternlab']);
+
+      // Recursively sort the manifests until dependency order is met...
+      while (TRUE) {
+        $modified = FALSE;
+        foreach ($manifests as $component => $manifest) {
+          $component_position = array_search($component, array_keys($manifests), TRUE);
+          if (isset($manifest['dependencies'])) {
+            foreach ($manifest['dependencies'] as $dependency => $version) {
+              $dependency_position = array_search($dependency, array_keys($manifests), TRUE);
+              if ($component_position < $dependency_position) {
+                $manifests = moveKeyBefore($manifests, $component, $dependency);
+                $modified = TRUE;
+                break 2;
+              }
+            }
+          }
+        }
+        if (!$modified) {
+          break;
+        }
+      }
+
+      $styles = '';
+      foreach (array_keys($manifests) as $manifest) {
+        $component = str_replace('@psu-online-education/', '', $manifest);
+        $potential_css_file = "node_modules/@psu-online-education/$component/dist/styles.css";
+        if (file_exists($potential_css_file)) {
+          $file_content = file_get_contents($potential_css_file);
+          // Strip out any UTF-8 BOM sequences before inlining.
+          $styles .= preg_replace("/^\xEF\xBB\xBF/", '', $file_content);
+        }
+      }
+      return $styles;
+  }));
+
+    $env->addFunction(new TwigFunction('get_component_scripts', function () {
+        $scripts = '';
+        foreach (glob('node_modules/@psu-online-education/*/dist/scripts.js') as $component) {
+            $scripts .= file_get_contents($component);
+        }
+        return $scripts;
+    }));
+
+    $env->addFunction(new TwigFunction('get_sprites', function() {
+        return file_get_contents('node_modules/@psu-online-education/sprite/dist/sprites.svg');
+    }));
+}
+
